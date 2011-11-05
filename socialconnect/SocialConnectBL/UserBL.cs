62b28ce03dd225cc221c99a240515d1ad14fa41c
jitendrac <jitendra.shah90@gmail.com>
2011-11-05 09:57:19 +0530

    Added WCF Service Project

---
+++ socialconnect/SocialConnectBL/UserBL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using SocialConnectDA;
+
+namespace SocialConnectBL
+{
+    public class UserBL
+    {
+        public static DataSet getAllElements()
+        {
+            return UserDA.getAllElements();
+        }
+    }
+}
---
+++ socialconnect/SocialConnectDA/UserDA.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.Common;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+using Microsoft.Practices.EnterpriseLibrary.Common;
+namespace SocialConnectDA
+{
+    public class UserDA
+    {
+        public static DataSet getAllElements()
+        {
+            DataSet ds = new DataSet();
+            Database db = DatabaseFactory.CreateDatabase();
+            String DbCommandStr = "Select * from [User]";
+            using (DbCommand dbCommand = db.GetSqlStringCommand(DbCommandStr))
+            {
+                ds = db.ExecuteDataSet(dbCommand);
+                dbCommand.Dispose();
+            }
+            return ds;
+        }
+    }
+}
